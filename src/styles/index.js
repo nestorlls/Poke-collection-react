2ab314b5fb1d710388f7788d7659a213b91429f4
Paul Llanque <llanquepol@gmail.com>
2023-03-16 09:42:32 -0500

    update package.json

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,5 +1,6 @@
 import { colors } from './colors';
-import { reset, global } from './global';
+import { reset, global } from './Global';
 import { typography } from './typography';
+import { colors } from './Colors';
 
 export { colors, reset, typography, global };
