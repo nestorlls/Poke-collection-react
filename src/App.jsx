f7003a9415307d895fbd39f9378aceb4383e97cd
Paul Llanque <llanquepol@gmail.com>
2023-04-29 13:00:23 -0500

    update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
+import styled from '@emotion/styled';
 import { AuthenticateApp } from './Authentication/AuthenticateApp';
 import { UnAuthenticateApp } from './Authentication/UnAuthenticateApp';
 import { useAuth } from './context/Auth-Context';
 
+const WraperApp = styled.main`
+  margin-top: 96px;
+  margin-bottom: 96px;
+
+  section.section {
+    max-width: 1024px;
+    padding-left: 16px;
+    padding-right: 16px;
+    margin-left: auto;
+    margin-right: auto;
+    height: 100%;
+  }
+`;
+
 function App() {
   const { user } = useAuth();
-  return user ? <AuthenticateApp /> : <UnAuthenticateApp />;
+  return (
+    <WraperApp>
+      <section className='section'>
+        {user ? <AuthenticateApp /> : <UnAuthenticateApp />}
+      </section>
+    </WraperApp>
+  );
 }
 
 export default App;
