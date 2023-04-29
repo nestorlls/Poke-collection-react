9ee7de5294ffe95400acb35672de68f0ee6dfaaa
Paul Llanque <llanquepol@gmail.com>
2023-04-29 12:59:18 -0500

    update PokemonData component

--- src/components/PokemonData/PokemonData.jsx
+++ src/components/PokemonData/PokemonData.jsx
@@ -1,11 +1,9 @@
 import styled from '@emotion/styled';
 import { MdFavorite } from 'react-icons/md';
-import { colors } from '../../styles/Colors';
+import { colors, typeColors } from '../../styles/Colors';
 import { typography } from '../../styles/typography';
-
-const PokeImg = styled.img`
-  max-width: 144px;
-`;
+import weight from '../../assets/weight.svg';
+import height from '../../assets/height.svg';
 
 const FavoriteButton = styled.button`
   display: flex;
@@ -19,11 +17,92 @@ const FavoriteButton = styled.button`
   font-family: ${typography.text};
   font-weight: bold;
   color: #fff;
+
+  div.btn-add__remove {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  }
+`;
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+
+  h2 {
+    ${typography.bold[24]}
+  }
+
+  p.format-id {
+    ${typography.bold[14]}
+  }
+
+  img {
+    max-width: 144px;
+  }
+
+  div.poke-type {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+  }
+
+  div.features {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    margin: 32px 0;
+
+    div.feature {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      gap: 12px;
+
+      div.feature-img {
+        width: 80px;
+        display: flex;
+        justify-content: center;
+        gap: 8px;
+
+        img {
+        }
+
+        span {
+          ${typography.regular[10]}
+        }
+      }
+
+      p.feature-letter {
+        ${typography.regular[8]}
+        color: ${colors.gray.medium}
+      }
+    }
+
+    hr {
+      border: none;
+      border-right: 1px solid ${colors.gray.medium};
+      height: 58px;
+    }
+  }
+`;
+
+const Type = styled.p`
+  ${typography.bold[10]}
+  background: ${({ type }) => typeColors[type]};
+  color: #fff;
+  padding: 2px 8px;
+  border-radius: 50px;
+  text-transform: capitalize;
 `;
 
 const formatId = (id) => {
-  id = String(id);
-  return id.lenght < 2 ? `00${id}` : id.lenght < 3 ? `$#0${id}` : `#${id}`;
+  return `#${id.toString().padStart(3, '0')}`;
 };
 
 export const PokemonData = ({
@@ -33,29 +112,49 @@ export const PokemonData = ({
   isFavorite,
 }) => {
   const RegularContent = (
-    <div>
+    <div className='btn-add__remove'>
       <MdFavorite color={colors.yellow[500]} />
-      Mark as favorite
+      <span>Mark as favorite</span>
     </div>
   );
 
   const FavoriteContent = (
-    <div>
+    <div className='btn-add__remove'>
       <MdFavorite color={colors.gray.light} />
-      Remove favorite
+      <span>Remove favorite</span>
     </div>
   );
 
   return (
-    <div>
-      <h2>{data.name}</h2>
-      <p>{formatId(data.id)}</p>
-      <PokeImg src={data.sprites.other['official-artwork'].front_default} />
-      {data.types.map((element) => (
-        <p key={element.slot}>{element.type.name}</p>
-      ))}
-      <p>Weight: {data.weight / 10} kg</p>
-      <p>Height: {data.height / 10} m</p>
+    <Card>
+      <h2 className='name'>{data.name}</h2>
+      <p className='format-id'>{formatId(data.id)}</p>
+      <img src={data.sprites.other['official-artwork'].front_default} />
+      <div className='poke-type'>
+        {data.types.map((element) => (
+          <Type key={element.slot} type={element.type.name}>
+            {element.type.name}
+          </Type>
+        ))}
+      </div>
+      <div className='features'>
+        <div className='feature'>
+          <div className='feature-img'>
+            <img src={weight} alt='weight' />
+            <span className='span'>{data.weight / 10} kg</span>
+          </div>
+          <p className='feature-letter'>Weight</p>
+        </div>
+        <hr />
+        <div className='feature'>
+          <div className='feature-img'>
+            <img src={height} alt='height' />
+            <span>{data.height / 10} m</span>
+          </div>
+          <p className='feature-letter'>Heidht</p>
+        </div>
+      </div>
+
       <FavoriteButton
         onClick={() =>
           isFavorite ? onRemoveFavorite(data) : onAddFavorite(data)
@@ -63,6 +162,6 @@ export const PokemonData = ({
       >
         {isFavorite ? RegularContent : FavoriteContent}
       </FavoriteButton>
-    </div>
+    </Card>
   );
 };
