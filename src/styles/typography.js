8dcd9fe005fafc4c373170fb107fb58cccc439d8
Paul Llanque <llanquepol@gmail.com>
2023-04-28 21:19:54 -0500

    update typography

--- src/styles/typography.js
+++ src/styles/typography.js
@@ -4,65 +4,58 @@ export const fonts = {
 };
 
 export const typography = {
-  text: {
-    xs: `
-    font-size: 0.75rem;
-    line-height: 1rem;
-    `,
-    sm: `
-    font-size: 0.875rem;
-    line-height: 1.25rem;
-    `,
-    md: `
-    font-size: 1rem;
-    line-height: 1.5rem;
-    `,
-    lg: `
-    font-size: 1.125rem;
-    line-height: 1.75rem;
-    `,
-    xl: `
-    font-size: 1.25rem;
-    line-height: 1.75rem;
-    `,
+  regular: {
+    8: {
+      'font-weight': '400;',
+      'font-size': '8px;',
+      'line-height': '12px;',
+    },
+    10: {
+      'font-weight': '400;',
+      'font-size': '10px;',
+      'line-height': '16px;',
+    },
+    12: {
+      'font-weight': '400;',
+      'font-size': '12px;',
+      'line-height': '16px;',
+    },
+    14: {
+      'font-weight': '400;',
+      'font-size': '14px;',
+      'line-height': '16px;',
+    },
+    24: {
+      'font-weight': '400;',
+      'font-size': '24px;',
+      'line-height': '32px;',
+    },
   },
-  head: {
-    xs: `
-    font-size: 1.25rem;
-    line-height: 1.75rem;
-    `,
-    sm: `
-    font-size: 1.5rem;
-    line-height: 2rem;
-    `,
-    md: `
-    font-size: 1.875rem;
-    line-height: 2.25rem;
-    `,
-    lg: `
-    font-size: 2.25rem;
-    line-height: 2.5rem;
-    `,
-    xl: `
-    font-size: 3rem;
-    line-height: 3rem;
-    `,
-    xxl: `
-    font-size: 3.75rem;
-    line-height: 3.75rem;
-    `,
+  bold: {
+    8: {
+      'font-weight': '700;',
+      'font-size': '8px;',
+      'line-height': '12px;',
+    },
+    10: {
+      'font-weight': '700;',
+      'font-size': '10px;',
+      'line-height': '16px;',
+    },
+    12: {
+      'font-weight': '700;',
+      'font-size': '12px;',
+      'line-height': '16px;',
+    },
+    14: {
+      'font-weight': '700;',
+      'font-size': '14px;',
+      'line-height': '16px;',
+    },
+    24: {
+      'font-weight': '700;',
+      'font-size': '24px;',
+      'line-height': '32px;',
+    },
   },
 };
-
-for (const size in typography.text) {
-  typography.text[size] += `
-  font-family: ${fonts.primary};
-  `;
-}
-
-for (const size in typography.head) {
-  typography.head[size] += `
-  font-family: ${fonts.secondary};
-  font-weight: 600;
-  `;
-}
